0b270700f882bffb17595c54d4e4ffa9bf079cfd
Lucas Debouté <lucas.deboute@gmail.com>
2019-08-28 18:04:15 +0200

    Working connection to database

--- cmd/purplewallet/main.go
+++ cmd/purplewallet/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import "github.com/jinzhu/configor"
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+import "log"
+import "strconv"
+
+var Config = struct {
+	Database struct {
+		Password string `required:"true" env:"DBPassword"`
+		User     string
+		Url      string
+		DBName   string
+		Port     uint `default:"3306"`
+	}
+}{}
 
 func main() {
-	fmt.Println("vim-go")
+	configor.Load(&Config, "config/config.yaml")
+	init_database()
+}
+
+func init_database() {
+	c_string := Config.Database.User + ":" + Config.Database.Password + "@tcp(" + Config.Database.Url + ":" + strconv.Itoa(Config.Database.Port) + ")/" + Config.Database.DBName + "?charset=utf8&parseTime=True"
+	log.Println("Connection to database started")
+	db, err := gorm.Open("mysql", c_string)
+	if err != nil {
+		log.Fatalln("Error occured: " + err.Error())
+	}
+	log.Println("Success")
+	db.LogMode(true)
+	defer db.Close()
 }
