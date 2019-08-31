1c353700eefaeca675aceb5794bd179c045fc417
Lucas Debouté <lucas.deboute@gmail.com>
2019-08-31 19:07:13 +0200

    Configuration var on config.go

---
+++ cmd/purplewallet/config.go
@@ -0,0 +1,12 @@
+package main
+
+var Config = struct {
+	Port     int `default:"8080"`
+	Database struct {
+		Password string `required:"true" env:"DBPassword"`
+		User     string
+		URL      string
+		DBName   string
+		Port     int `default:"3306"`
+	}
+}{}
