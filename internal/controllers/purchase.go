4ab5654353a44746cbdb804b905824ad64ab5ab6
Lucas Debouté <lucas.deboute@gmail.com>
2019-09-03 12:02:26 +0200

    Updated Purchase function, now checks if user/wallet/users_wallets exists in database and can register

--- internal/controllers/purchase.go
+++ internal/controllers/purchase.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"log"
 	"net/http"
 	"purplewallet/internal/database"
 	"purplewallet/internal/models"
@@ -16,18 +15,53 @@ func Purchase(r *gin.RouterGroup) {
 	r.GET("/:username", getPurchaseByName)
 }
 
+func isUserInDatabase(id int) bool {
+	var count int
+	database.GetDatabase().Table("users").Where("id = ?", id).Count(&count)
+	if count == 0 {
+		return false
+	}
+	return true
+}
+
+func isWalletInDatabase(id int) bool {
+	var count int
+	database.GetDatabase().Table("wallets").Where("id = ?", id).Count(&count)
+	if count == 0 {
+		return false
+	}
+	return true
+}
+
+func checkIfUserCanRegister(idUser int, idWallet int) bool {
+	var count int
+	database.GetDatabase().Table("users_wallets").Where("users_id = ? AND wallets_id = ?", idUser, idWallet).Count(&count)
+	if count == 0 {
+		return false
+	}
+	return true
+}
+
 func addNewPurchase(c *gin.Context) {
 	sum, err := strconv.ParseFloat(c.PostForm("sum"), 64)
 	reason := c.PostForm("reason")
-	userID := c.PostForm("user_id")
-	log.Printf("%f, %s, %s", sum, reason, userID)
-	if err != nil || reason == "" || userID == "" {
+	userID, _ := strconv.ParseInt(c.DefaultPostForm("user_id", "0"), 10, 32)
+	wallet, _ := strconv.ParseInt(c.DefaultPostForm("wallet", "0"), 10, 32)
+	owedBy, _ := strconv.ParseInt(c.DefaultPostForm("owedBy", "0"), 10, 32)
+	if err != nil || reason == "" || userID == 0 || wallet == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Missing parameters"})
 		return
 	}
-	purchase := models.Purchase{Sum: sum, Reason: reason, Date: time.Now(), OwedBy: 0, CategoriesID: 0, UserID: 0}
+	if isUserInDatabase(int(userID)) == false ||
+		isWalletInDatabase(int(wallet)) == false ||
+		checkIfUserCanRegister(int(userID), int(wallet)) == false {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Missing parameters"})
+		return
+	}
+
+	purchase := models.Purchase{Sum: sum, Reason: reason, Date: time.Now(), OwedBy: int(owedBy), CategoriesID: 0, UserID: 0}
 	database.GetDatabase().Create(&purchase)
-	c.String(http.StatusAccepted, "oui")
+	c.String(http.StatusAccepted, "Success")
 }
 
 // getPurchaseByName is used to return a list of purchases from an specific user
