65f7e793ce1f6bac0cc8ca8c52ee6552619de44d
Lucas Debouté <lucas.deboute@gmail.com>
2019-09-08 17:49:57 +0200

    Added serializable models

---
+++ internal/serializable/userbalance.go
@@ -0,0 +1,10 @@
+package serializable
+
+// UserBalance is used to contain a resume of the user dispences
+type UserBalance struct {
+	UserID          int
+	CommonPurchases float64
+	Debts           float64
+	Total           float64
+	Balance         float64
+}
